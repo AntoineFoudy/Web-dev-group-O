dede9ce9353788f0222d226229decf16f60cca4d
ChristianJ123ABC <x23524103@student.ncirl.ie>
2023-12-06 23:27:43 +0000

    Background buttons + NAV bar additions  + task list / report
    Added more backgrounds by adding 3 buttons to choose between 3 backgrounds.
    
    Added credentials and the buttons to the nav bar
    
    Did some other slight changes
     (click message to red, etc)
    
    Added task list + report

--- gallery.js
+++ gallery.js
@@ -7,10 +7,31 @@
 */
 
 /// These lines are used to create a background, resize it to a certain resolution and then make it in a fixed position so it doesn't move during scrolling ///
+
+/*Default background (image 1) */
 document.body.style.background = "url('galleryphotos/yellowstonegrass.jpg') no-repeat";
 document.body.style.backgroundSize = "1920px 1080px";  /// readjusts the background image to 1920x1080 resolution
 document.body.style.backgroundAttachment = "fixed";
 
+
+function backgroundImage1(){
+	document.body.style.background = "url('galleryphotos/yellowstonegrass.jpg') no-repeat";
+	document.body.style.backgroundSize = "1920px 1080px";  /// readjusts the background image to 1920x1080 resolution
+	document.body.style.backgroundAttachment = "fixed";
+}
+
+function backgroundImage2(){
+	document.body.style.background = "url('galleryphotos/hotspring.jpg') no-repeat";
+	document.body.style.backgroundSize = "1920px 1080px";  /// readjusts the background image to 1920x1080 resolution
+	document.body.style.backgroundAttachment = "fixed";
+}
+
+function backgroundImage3(){
+	document.body.style.background = "url('galleryphotos/snowlake.jpg') no-repeat";
+	document.body.style.backgroundSize = "1920px 1080px";  /// readjusts the background image to 1920x1080 resolution
+	document.body.style.backgroundAttachment = "fixed";
+	
+}
 
 
 
@@ -20,3 +41,5 @@ document.body.style.backgroundAttachment = "fixed";
    https://www.w3schools.com/jsref/prop_style_backgroundsize.asp
    https://www.w3schools.com/jsref/prop_style_backgroundattachment.asp
 */
+
+
